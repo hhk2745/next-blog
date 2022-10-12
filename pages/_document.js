dd7401b714d78a88487f103f7052e91d49ad7592
김한호 <fj2746@gmail.com>
2022-10-12 17:08:46 +0900

    edit

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <script src="https://kit.fontawesome.com/2398f113df.js" crossorigin="anonymous"></script>
+        {/* <script src="https://kit.fontawesome.com/2398f113df.js" crossOrigin="anonymous"></script> */}
 
       </Head>
       <body>
